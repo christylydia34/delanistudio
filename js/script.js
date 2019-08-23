5e314e7269baafb2d4fab53d5c1528a848915297
christylydia <christinegikandi@gmail.com>
2019-08-23 16:54:14 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-$(".clickable").click(function(){
+$(".clickable1").click(function(){
     $(".design hide").toggle();
     $(".design show").toggle();
 });
 $(document).ready(function(){
-    $(".clickable").click(function(){
+    $(".clickable2").click(function(){
         $(".development hide").toggle();
         $(".development show").toggle();
     });
 
     $(document).ready(function(){
-        $(".clickable").click(function(){
+        $(".clickable3").click(function(){
             $(".pm hide").toggle();
             $(".pm show").toggle();
         });
