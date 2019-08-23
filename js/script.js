1b903262591c5a6e55edfe503f03bd3a9348babe
christylydia <christinegikandi@gmail.com>
2019-08-23 14:26:20 +0300

    add to js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ $(".clickable").click(function(){
     $(".design hide").toggle();
     $(".design show").toggle();
 });
+$(document).ready(function(){
+    $(".clickable").click(function(){
+        $(".development hide").toggle();
+        $(".development show").toggle();
+    });
 
-
+    $(document).ready(function(){
+        $(".clickable").click(function(){
+            $(".pm hide").toggle();
+            $(".pm show").toggle();
+        });
+        
+    });
+});     
 });
